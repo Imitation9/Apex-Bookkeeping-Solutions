ba37229a1875472d704216f66037f71e7c2d4ed2
Imitation9 <ryoung4696@gmail.com>
2026-07-23 16:28:34 -0500

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,1 +1,13 @@
+import Navigation from "./Navigation";
 
+export default function Header() {
+  return (
+    <header className="sticky top-0 z-50 backdrop-blur-md bg-white/85 shadow-sm">
+      <div className="container flex h-20 items-center justify-between">
+
+        <Navigation />
+
+      </div>
+    </header>
+  );
+}
