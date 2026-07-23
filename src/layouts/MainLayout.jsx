8960aa61054328596b143edbfc5a616ab86320e8
Imitation9 <ryoung4696@gmail.com>
2026-07-23 16:27:46 -0500

    Update MainLayout.jsx

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,1 +1,18 @@
+import { Outlet } from "react-router-dom";
 
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+
+export default function MainLayout() {
+  return (
+    <div className="min-h-screen flex flex-col bg-apex-light">
+      <Header />
+
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
