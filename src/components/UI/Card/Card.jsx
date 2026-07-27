d3f7e63c87d2378a92a711d5beb8d676c3325c8d
Imitation9 <ryoung4696@gmail.com>
2026-07-27 14:49:12 -0500

    Update and rename card to Card.jsx

---
+++ src/components/UI/Card/Card.jsx
@@ -0,0 +1,24 @@
+export default function Card({
+  children,
+  className = "",
+}) {
+  return (
+    <div
+      className={`
+        rounded-3xl
+        border
+        border-slate-200
+        bg-white
+        p-8
+        shadow-lg
+        transition-all
+        duration-300
+        hover:-translate-y-2
+        hover:shadow-2xl
+        ${className}
+      `}
+    >
+      {children}
+    </div>
+  );
+}
