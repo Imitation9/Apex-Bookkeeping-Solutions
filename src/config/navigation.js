1036b027747486fcf63ef8819777e96847c0b1bf
Imitation9 <ryoung4696@gmail.com>
2026-07-27 14:57:59 -0500

    Update and rename config to navigation.js

---
+++ src/config/navigation.js
@@ -0,0 +1,14 @@
+export const navigation = [
+  {
+    title: "Services",
+    href: "#services",
+  },
+  {
+    title: "Industries",
+    href: "#industries",
+  },
+  {
+    title: "About",
+    href: "#about",
+  },
+];
