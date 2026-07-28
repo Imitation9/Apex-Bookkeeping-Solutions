28733805951d5b749cbad28938cb36f196c828b3
Imitation9 <ryoung4696@gmail.com>
2026-07-28 14:18:18 -0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,17 +2,16 @@ import Hero from "../components/sections/Hero/Hero";
 import TrustBar from "../components/sections/Hero/TrustBar";
 import Services from "../components/sections/Services/Services";
 import About from "../components/sections/About/About";
+import Audiences from "../components/sections/Audiences/Audiences";
 
 export default function Home() {
   return (
     <>
       <Hero />
-
       <TrustBar />
-
       <Services />
-
       <About />
+      <Audiences />
     </>
   );
 }
