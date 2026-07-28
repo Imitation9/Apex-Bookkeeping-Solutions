a9c1a077462b4d9a235b83fd45dd805f7283cbcc
Imitation9 <ryoung4696@gmail.com>
2026-07-28 14:08:10 -0500

    Create Home.jsx

---
+++ src/components/sections/Hero/Home.jsx
@@ -0,0 +1,12 @@
+import Hero from "../components/sections/Hero/Hero";
+import TrustBar from "../components/sections/Hero/TrustBar";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+
+      <TrustBar />
+    </>
+  );
+}
