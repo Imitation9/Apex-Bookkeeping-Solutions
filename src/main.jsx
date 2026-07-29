3978461a9bd8f181485f7888563f129789125bdd
Imitation9 <ryoung4696@gmail.com>
2026-07-29 15:09:13 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./App.jsx";
+import App from "./App";
+import "./styles/globals.css";
+import "./styles/animations.css";
 
-import "./styles/global.css";
-
-
-ReactDOM
-.createRoot(
-document.getElementById("root")
-)
-.render(
-
-<React.StrictMode>
-
-<App/>
-
-</React.StrictMode>
-
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
