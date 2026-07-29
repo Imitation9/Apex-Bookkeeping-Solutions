60ae0946551dc4f5351d9f21c901617842f65d89
Imitation9 <ryoung4696@gmail.com>
2026-07-29 13:41:50 -0500

    Create footerNavigation.js

---
+++ src/data/footerNavigation.js
@@ -0,0 +1,39 @@
+export const footerNavigation = {
+  company: [
+    {
+      label: "Why Apex",
+      href: "#about",
+    },
+    {
+      label: "Who We Serve",
+      href: "#industries",
+    },
+    {
+      label: "Our Process",
+      href: "#process",
+    },
+    {
+      label: "FAQ",
+      href: "#faq",
+    },
+  ],
+
+  services: [
+    {
+      label: "Bookkeeping",
+      href: "#services",
+    },
+    {
+      label: "Book Cleanup",
+      href: "#services",
+    },
+    {
+      label: "Payroll",
+      href: "#services",
+    },
+    {
+      label: "QuickBooks Consulting",
+      href: "#services",
+    },
+  ],
+};
