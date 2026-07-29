ca583784446712ca8043b866bb6e0b127d809eb2
Imitation9 <ryoung4696@gmail.com>
2026-07-29 17:37:25 -0500

    Update Header.jsx

--- src/components/layouts/Header/Header.jsx
+++ src/components/layouts/Header/Header.jsx
@@ -4,10 +4,12 @@ import MobileMenu from "../MobileMenu/MobileMenu";
 
 export default function Header() {
   return (
-    <header>
-      <Logo />
-      <Navigation />
-      <MobileMenu />
+    <header className="relative z-50 border-b border-slate-200 bg-white">
+      <div className="mx-auto flex max-w-7xl items-center justify-between px-6 py-4 lg:px-8">
+        <Logo />
+        <Navigation />
+        <MobileMenu />
+      </div>
     </header>
   );
 }
