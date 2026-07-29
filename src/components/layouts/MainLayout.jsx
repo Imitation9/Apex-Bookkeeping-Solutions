d72073a04614a5466c1dde772bc173459c472913
Imitation9 <ryoung4696@gmail.com>
2026-07-29 13:45:15 -0500

    Update MainLayout.jsx

--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 
 import Header from "../components/layout/Header/Header";
+import Footer from "../components/layout/Footer/Footer";
 
 export default function MainLayout() {
   return (
@@ -10,6 +11,8 @@ export default function MainLayout() {
       <main>
         <Outlet />
       </main>
+
+      <Footer />
     </>
   );
 }
