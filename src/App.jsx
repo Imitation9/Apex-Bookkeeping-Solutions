e31559144fcd7e51c0bab91c10764e3a8a38ddff
Imitation9 <ryoung4696@gmail.com>
2026-07-29 14:56:59 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,22 @@
 import {
-BrowserRouter,
-Routes,
-Route
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import MainLayout from "./components/layouts/MainLayout";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <BrowserRouter basename="/Apex-Bookkeeping-Solutions">
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
-from "react-router-dom";
-
-
-import MainLayout
-from "./layouts/MainLayout";
-
-
-import Home
-from "./pages/Home";
-
-
-function App(){
-
-
-return(
-
-<BrowserRouter>
-
-
-<Routes>
-
-
-<Route
-path="/"
-element={<MainLayout/>}
->
-
-
-<Route
-index
-element={<Home/>}
-/>
-
-
-</Route>
-
-
-</Routes>
-
-
-</BrowserRouter>
-
-);
-
-
-}
-
 
 export default App;
