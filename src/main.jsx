547b0977b508e3ca8bf665f9a567ca75280f42dc
Imitation9 <ryoung4696@gmail.com>
2026-07-30 14:45:48 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+
+import "./styles/variables.css";
 import "./styles/globals.css";
+import "./styles/utilities.css";
 import "./styles/animations.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
