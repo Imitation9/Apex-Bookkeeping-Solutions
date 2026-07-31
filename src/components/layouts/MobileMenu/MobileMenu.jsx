403808ab4d4b818463803605feadcb67a4075bfe
Imitation9 <ryoung4696@gmail.com>
2026-07-31 16:05:39 -0500

    Update MobileMenu.jsx

--- src/components/layouts/MobileMenu/MobileMenu.jsx
+++ src/components/layouts/MobileMenu/MobileMenu.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import { navigation } from "../../../config/navigation";
+import { SITE } from "../../../config/site";
 
 export default function MobileMenu() {
   const [isOpen, setIsOpen] = useState(false);
