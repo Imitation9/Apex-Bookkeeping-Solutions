b30650f651763d92315ca70596a9bc7a0beb715a
Imitation9 <ryoung4696@gmail.com>
2026-08-02 14:43:50 -0500

    Update BrandWatermark.jsx

--- src/components/UI/BrandWatermark/BrandWatermark.jsx
+++ src/components/UI/BrandWatermark/BrandWatermark.jsx
@@ -5,15 +5,15 @@ export default function BrandWatermark({
   position = "right",
 }) {
   const positions = {
-    left: "-left-20 top-1/2 -translate-y-1/2",
+    left: "-left-24 top-1/2 -translate-y-1/2",
     center: "left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2",
-    right: "-right-20 top-1/2 -translate-y-1/2",
+    right: "-right-24 top-1/2 -translate-y-1/2",
   };
 
   return (
     <div
       className={[
-        "brand-watermark pointer-events-none absolute -z-10 select-none",
+        "pointer-events-none absolute -z-10 select-none",
         positions[position] ?? positions.right,
         className,
       ].join(" ")}
@@ -22,7 +22,16 @@ export default function BrandWatermark({
       <img
         src={apexMark}
         alt=""
-        className="h-auto w-full"
+        className="
+          h-auto
+          w-[34rem]
+          xl:w-[40rem]
+          2xl:w-[46rem]
+          opacity-[0.035]
+          blur-[0.4px]
+          animate-float-slow
+        "
+        draggable="false"
       />
     </div>
   );
