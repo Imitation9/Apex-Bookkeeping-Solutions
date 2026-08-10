959d222f89079170ac61a8b065c5fa7370fb774b
Imitation9 <ryoung4696@gmail.com>
2026-08-10 13:25:50 -0500

    Update site.js

--- src/config/site.js
+++ src/config/site.js
@@ -1,14 +1,11 @@
 export const SITE = {
   name: "Apex Bookkeeping Solutions, LLC",
-
   shortName: "Apex Bookkeeping Solutions",
 
   phoneDisplay: "501.366.8940",
-
   phoneHref: "tel:5013668940",
 
   emailDisplay: "robert@apexbksolutions.com",
-
   emailHref: "mailto:robert@apexbksolutions.com",
 
   calendly: "https://calendly.com/ryoung4696",
