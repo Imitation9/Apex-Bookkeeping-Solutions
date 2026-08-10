9aed4fa420aa9fd2069343fa2093c37bf7570177
Imitation9 <ryoung4696@gmail.com>
2026-08-10 13:13:27 -0500

    Update site.js
    Add email address to page.

--- src/config/site.js
+++ src/config/site.js
@@ -1,8 +1,17 @@
 export const SITE = {
   name: "Apex Bookkeeping Solutions, LLC",
+
   shortName: "Apex Bookkeeping Solutions",
+
   phoneDisplay: "501.366.8940",
+
   phoneHref: "tel:5013668940",
+
+  emailDisplay: "robert@apexbksolutions.com",
+
+  emailHref: "mailto:robert@apexbksolutions.com",
+
   calendly: "https://calendly.com/ryoung4696",
+
   location: "North Little Rock, Arkansas",
 };
